708bee85d133a6526d84e4ed810dfc94b837c5d2
FuryForever <messiasfuryforever@gmail.com>
2023-07-10 13:25:31 -0300

    Toaster tests

--- src/components/answerInput/AnswerInput.tsx
+++ src/components/answerInput/AnswerInput.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import styles from './answerInput.module.css'
+import toast, { Toaster } from 'react-hot-toast'
+
+const errorNotify = () => toast.error("Wrong answer!")
 
 export default function AnswerInput() {
   return (
-    <form onSubmit={(e) => {e.preventDefault()}}className={styles.form}>
-      <input type="text" placeholder="Answer..."/>
-      <input type="submit" value="Try"/>
-    </form>
+    <>
+      <Toaster position='top-right' toastOptions={{ duration: 1000, style: { background: '#333', color: '#fff' } }}/>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        errorNotify()
+      }} className={styles.form}>
+        <input type="text" placeholder="Answer..."/>
+        <input type="submit" value="Try"/>
+      </form>
+    </>
     )
 }
