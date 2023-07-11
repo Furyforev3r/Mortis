64242623038fef0b4bb0997b39c0fe4edc53d7db
FuryForever <messiasfuryforever@gmail.com>
2023-07-11 11:31:21 -0300

    Verify answers system

--- src/components/answerInput/AnswerInput.tsx
+++ src/components/answerInput/AnswerInput.tsx
@@ -1,19 +1,36 @@
 "use client";
 
+import { FormEvent, useState } from 'react';
 import styles from './answerInput.module.css'
 import toast, { Toaster } from 'react-hot-toast'
+import Enigmas, { EnigmaInterface } from './Enigmas';
+import { useRouter } from 'next/navigation';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
 
 const errorNotify = () => toast.error("Wrong answer!")
-
 export default function AnswerInput() {
+  const [Answer, setAnswer] = useState("")
+  const router: AppRouterInstance = useRouter()
+
+
+  function Verify(e: FormEvent<HTMLFormElement>, answerSent: string) {
+    e.preventDefault()
+    const filtered: Array<EnigmaInterface> = Enigmas.filter((Enigma: EnigmaInterface) => Enigma.answer == answerSent)
+
+    if (filtered.length > 0) {
+      router.push("enigma/"+filtered[0].nextUrl)
+    } else {
+      errorNotify()
+    }
+  }
+
   return (
     <>
       <Toaster position='top-right' toastOptions={{ duration: 1000, style: { background: '#333', color: '#fff' } }}/>
       <form onSubmit={(e) => {
-        e.preventDefault();
-        errorNotify()
+        Verify(e, Answer)
       }} className={styles.form}>
-        <input type="text" placeholder="Answer..."/>
+        <input type="text" placeholder="Answer..." onChange={(e) => setAnswer(e.target.value)}/>
         <input type="submit" value="Try"/>
       </form>
     </>
---
+++ src/components/answerInput/Enigmas.ts
@@ -0,0 +1,8 @@
+export interface EnigmaInterface {
+  answer: string,
+  nextUrl: string,
+}
+
+const Enigmas: Array<EnigmaInterface> = [{answer: "ifnothingmakessense", nextUrl: "reflection"}]
+
+export default Enigmas
